cb0210702533f1b1defad0be9ebd9c03ca556f30
Dairan S. <dairandemora.work@gmail.com>
2025-02-01 18:13:25 -0400

    UX/UI mejorada.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TaskListComponent } from './components/task-list/task-list.component';
 import { CommonModule } from '@angular/common';
+import { NavbarComponent } from "./components/navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TaskListComponent, CommonModule],
+  imports: [RouterOutlet, TaskListComponent, CommonModule, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
